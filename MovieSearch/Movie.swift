417259debaa89171bcddb48172a3bad24c77dac6
edmund-h <447ght@gmail.com>
2017-02-14 18:08:58 -0500

    done with @tjcarney89

---
+++ MovieSearch/Movie.swift
@@ -0,0 +1,38 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Edmund Holderbaum on 2/14/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    let title: String
+    let year: String
+    let id: String
+    let type: String
+    let link: String
+    
+    init (_ search: [String : Any]) {
+        self.title = search["Title"] as! String
+        self.year = search["Year"] as! String
+        self.id = search["imdbID"] as! String
+        self.type = search["Type"] as! String
+        self.link = search["Poster"] as! String
+    }
+    
+}
+
+
+let bullshitMovie: [String : Any] = [
+    "Title" : "My Bullshit Movie",
+    "Year" : 2016,
+    "imdbID" : "12XU",
+    "Type" : "BullshitMovie",
+    "Poster" : "Goatse.cx"
+]
+
+var bsMovie = Movie(bullshitMovie)
+
--- MovieSearch/MovieSearchViewController.swift
+++ MovieSearch/MovieSearchViewController.swift
@@ -8,27 +8,73 @@
 
 import UIKit
 
-class MovieSearchViewController: UIViewController {
+class MovieSearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var movieTableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
     
     let movieAPI = MovieAPI()
-    
-    @IBOutlet weak var searchTextField: UITextField!
+    var searchResultsDict: [String : Any] = [:]
+    var movieResults: [Movie] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         searchTextField.delegate = self
+        movieTableView.delegate = self
+        movieTableView.dataSource = self
     }
     
     func searchFilm(with title: String) {
+        movieResults.removeAll()
+        guard let temp = movieAPI.searchFilm(title: title) else { return }
+        searchResultsDict = temp
+        var movieResultsArray = searchResultsDict["Search"]
+        for myMovie in movieResultsArray as! [[String : Any]] {
+            movieResults.append(Movie(myMovie as! [String : Any]))
+        }
+        print("MOVIE RESULTS: \(movieResults)")
+        //print(temp)
+        //print (temp.count)
+        
+        movieTableView.reloadData()
+
+        
+        
+//        tableView(tableView(self.tableView, cellForRowAt: <#T##IndexPath#>))
         
         print(title)
         
-       print(movieAPI.searchFilm(title: title))
+       //print(movieAPI.searchFilm(title: title))
         
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieSearchCell
+        let currentMovie = movieResults[indexPath.row]
+        cell.titleLabel.text = currentMovie.title
+        cell.yearLabel.text = currentMovie.year
+        cell.idLabel.text = currentMovie.id
+        cell.urlLabel.text = currentMovie.link
+        
+        
+        
+        return cell
+    }
 }
+
+
+
+
+/*Create a separate Movie type that has an init function which takes in a Dictionary of type [String : Any] which its able to parse through to setup its various stored properties.
+Create a UITableView in the MovieSearchViewController (you can use Storyboard and are encouraged to do so).
+Set up this UITableView to display all of the necessary info of all of the Movie objects you were able to create as a result of the search done by the user.*/
+
+
 
 // MARK: - UITextFieldDelegate
 extension MovieSearchViewController: UITextFieldDelegate {
